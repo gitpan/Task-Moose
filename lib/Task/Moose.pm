efc864b3e5cfd0db950b37b9d2970b9a6fbbf30b
Jesse Luehrs <doy@cpan.org>
2009-09-17 23:09:32 +0000

    Import of DOY/Task-Moose-0.02 from CPAN.
    gitpan-cpan-distribution: Task-Moose
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DOY/Task-Moose-0.02.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

--- lib/Task/Moose.pm
+++ lib/Task/Moose.pm
@@ -1,6 +1,6 @@
 package Task::Moose;
 
-our $VERSION   = '0.01';
+our $VERSION   = '0.02';
 our $AUTHORITY = 'cpan:STEVAN';
 
 1;
@@ -19,130 +19,187 @@ This Task installs Moose and then optionally installs a number of
 Moose extensions listed below. This list is meant to be comprehensive, 
 so if I missed something please let me know.
 
-=over 4
+=head1 MODULES
 
-=item I<Autoboxing support>
-    
-L<Moose::Autobox>
+=head2 Make Moose Stricter
 
-=item I<Extending Moose attributes with basic Perl types>
-    
-L<MooseX::AttributeHelpers>
-    
-=item I<Class attributes for Moose>
+=head3 L<MooseX::StrictConstructor>
 
-L<MooseX::ClassAttribute>
+Making Moose constructors stricter
 
-=item I<Support for PBP style accessors>
+=head3 L<MooseX::Params::Validate>
 
-L<MooseX::Policy::SemiAffordanceAccessor>
+Moose-ish method parameter handling
 
-=item I<Moose-ish method parameter handling>
-    
-L<MooseX::Params::Validate>
-    
-=item I<Moose-ish methods>
+=head2  Traits / Roles
 
-L<MooseX::Method>
+=head3 L<MooseX::Role::TraitConstructor>
 
-=item I<Making Moose constructors stricter>
-    
-L<MooseX::StrictConstructor>
-    
-=item I<Globref instance type for Moose>
-    
-L<MooseX::GlobRef::Object>
-    
-=item I<Inside-Out instance type for Moose>
+Runtime trait application in constructors
 
-L<MooseX::InsideOut>
+=head3 L<MooseX::Traits>
 
-=item I<CGI-style parameter role>
-    
-L<MooseX::Param>
-    
-=item I<Singleton support role>
+Easy creation of objects with traits
 
-L<MooseX::Singleton>
-    
-=item I<Moose-ish Iterator support role>
+=head3 L<MooseX::Object::Pluggable>
 
-L<MooseX::Iterator>
-    
-=item I<Moose-ish plugin system>
+Moose-ish plugin system
 
-L<MooseX::Object::Pluggable>
+=head3 L<MooseX::Role::Parameterized>
 
-=item I<Moose type extensions>
-    
-L<MooseX::Types>
+Parameterized roles
 
-=item I<Path::Class Moose type extension>
-    
-L<MooseX::Types::Path::Class>
-    
-=item I<Set::Object Moose type extension>
-    
-L<MooseX::Types::Set::Object>
+=head2 Instance Types
 
-=item I<Moose serialization>
-    
-L<MooseX::Storage>
+=head3 L<MooseX::GlobRef>
 
-=item I<Better script writing with Moose>
-    
-L<MooseX::Getopt>
+Globref instance type for Moose
 
-=item I<Support for config with MooseX::Getopt>
-    
-L<MooseX::ConfigFromFile>
-    
-=item I<Config file support for MooseX::Getopt with Config::Any>
-    
-L<MooseX::SimpleConfig>
-    
-=item I<LogDispatch support for Moose>
-    
-L<MooseX::LogDispatch>
+=head3 L<MooseX::InsideOut>
 
-=item I<lazy loaded LogDispatch support for Moose>
+Inside out instance type for Moose
 
-L<MooseX::LazyLogDispatch>
+=head3 L<MooseX::Singleton>
 
-=item I<App::Cmd integration for Moose>
-    
-L<MooseX::App::Cmd>
-    
-=item I<Daemonization support roles for Moose>
-    
-L<MooseX::Daemonize>
+Singleton instance type for Moose
 
-=item I<Easily wrap command line apps with Moose>
-    
-L<MooseX::Role::Cmd>
+=head3 L<MooseX::NonMoose>
 
-=item I<Base role for Async Moose modules>
-    
-L<MooseX::Async>
-    
-=item I<POE and Moose>
+Subclassing of non-Moose classes
 
-L<MooseX::POE>
-    
-=item I<Sub-process management for asynchronous tasks using Moose and POE>
-    
-L<MooseX::Workers>
+=head2 Declarative Syntax
 
-=item I<experimental Moose Compiler tools>
-    
-L<MooseX::Compile>
-L<MooseX::Compile::CLI>
+=head3 L<MooseX::Declare>
 
-=item I<Moose support tools>
-    
-L<Pod::Coverage::Moose>
+Declarative syntax with L<Devel::Declare>
 
-=back
+=head3 L<MooseX::Method::Signatures>
+
+Declarative method syntax
+
+=head3 L<TryCatch>
+
+Declarative exception handling
+
+=head2 Types
+
+=head3 L<MooseX::Types>
+
+Moose type extensions
+
+=head3 L<MooseX::Types::Structured>
+
+Structured type constraints
+
+=head3 L<MooseX::Types::Path::Class>
+
+L<Path::Class> Moose type extension
+
+=head3 L<MooseX::Types::Set::Object>
+
+L<Set::Object> Moose type extension
+
+=head3 L<MooseX::Types::DateTime>
+
+L<DateTime> Moose type extension
+
+=head2 Command Line Integration
+
+=head3 L<MooseX::Getopt>
+
+Better script writing with Moose
+
+=head3 L<MooseX::ConfigFromFile>
+
+Support for config with L<MooseX::Getopt>
+
+=head3 L<MooseX::SimpleConfig>
+
+Config file support for L<MooseX::Getopt> with L<Config::Any>
+
+=head3 L<MooseX::App::Cmd>
+
+L<App::Cmd> integration for Moose
+
+=head3 L<MooseX::Role::Cmd>
+
+Easily wrap command line apps with Moose
+
+=head2 Logging
+
+=head3 L<MooseX::LogDispatch>
+
+L<Log::Dispatch> support for Moose
+
+=head3 L<MooseX::LazyLogDispatch>
+
+Lazy loaded L<Log::Dispatch> support for Moose
+
+=head3 L<MooseX::Log::Log4perl>
+
+L<Log::Log4perl> support for Moose
+
+=head2 Async
+
+=head3 L<MooseX::POE>
+
+Moose wrapped L<POE>
+
+=head3 L<MooseX::Workers>
+
+Sub-process management for asynchronous tasks using Moose and L<POE>
+
+=head2 Utility Roles
+
+=head3 L<MooseX::Daemonize>
+
+Daemonization support roles for Moose
+
+=head3 L<MooseX::Param>
+
+CGI-style parameter role
+
+=head3 L<MooseX::Iterator>
+
+Moose-ish iterator support role
+
+=head3 L<MooseX::Clone>
+
+More robust and flexible cloning support
+
+=head3 L<MooseX::Storage>
+
+Moose serialization
+
+=head2 Other Useful Extensions
+
+=head3 L<Moose::Autobox>
+
+L<Autoboxing|autobox> support
+
+=head3 L<MooseX::ClassAttribute>
+
+Class attributes for Moose
+
+=head3 L<MooseX::SemiAffordanceAccessor>
+
+Support for PBP style accessors
+
+=head3 L<namespace::autoclean>
+
+Keep imported subroutines out of your class's namespace
+
+=head2 Utilities
+
+=head3 L<Pod::Coverage::Moose>
+
+L<Pod::Coverage> extension for Moose
+
+=head1 NOTES
+
+L<MooseX::AttributeHelpers> has been removed from this list because
+its functionality has been subsumed into L<Moose> itself. See
+L<Moose::Meta::Attribute::Native> for more details.
 
 =head1 BUGS
 
@@ -154,9 +211,13 @@ to cpan-RT.
 
 Stevan Little E<lt>stevan.little@iinteractive.comE<gt>
 
+Jesse Luehrs E<lt>doy at tozt dot netE<gt>
+
+Chris Prather E<lt>chris@prather.orgE<gt>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 Infinity Interactive, Inc.
+Copyright 2008-2009 Infinity Interactive, Inc.
 
 L<http://www.iinteractive.com>
 
